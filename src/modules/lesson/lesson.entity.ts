6d78ee8de7cfb644f1cffafc82f45abed01dce79
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-04-07 22:21:18 -0300

    feat: Define a entidade Lesson

--- src/modules/lesson/lesson.entity.ts
+++ src/modules/lesson/lesson.entity.ts
@@ -1,1 +1,39 @@
-export class Lesson {}
+import { ObjectId } from 'mongodb';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ObjectIdColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'lessons' })
+export class Lesson {
+  @ObjectIdColumn()
+  id: ObjectId;
+
+  @Column({ name: 'title' })
+  title: string;
+
+  @Column({ name: 'start_time' })
+  startTime: Date;
+
+  @Column({ name: 'end_time' })
+  endTime: Date;
+
+  @Column({ name: 'lesson_date' })
+  lessonDate: Date;
+
+  @Column({ name: 'observations' })
+  observations: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
+}
