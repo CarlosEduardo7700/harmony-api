f7faa695942952e9e8f536b1cf0a58bd64dd429e
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-04-07 21:51:23 -0300

    feat: Gera o módulo de Lesson com CRUD generator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfigService } from './database/config/database.config.service';
+import { LessonModule } from './modules/lesson/lesson.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { DatabaseConfigService } from './database/config/database.config.service
       useClass: DatabaseConfigService,
       inject: [DatabaseConfigService],
     }),
+    LessonModule,
   ],
   controllers: [],
   providers: [],
