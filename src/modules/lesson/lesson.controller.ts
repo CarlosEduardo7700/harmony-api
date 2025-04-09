bc5ddb2706b2249b5145539ca672aff4a3bd01d2
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-04-08 21:58:36 -0300

    feat: Implementa a funcionalidade de cadastrar aulas

--- src/modules/lesson/lesson.controller.ts
+++ src/modules/lesson/lesson.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import { Controller, Post, Body } from '@nestjs/common';
 import { LessonService } from './lesson.service';
 import { CreateLessonDto } from './dto/create-lesson.dto';
-import { UpdateLessonDto } from './dto/update-lesson.dto';
 
 @Controller('lesson')
 export class LessonController {
   constructor(private readonly lessonService: LessonService) {}
 
   @Post()
-  create(@Body() createLessonDto: CreateLessonDto) {
-    return this.lessonService.create(createLessonDto);
-  }
+  async create(@Body() createLessonDto: CreateLessonDto) {
+    const lesson = await this.lessonService.create(createLessonDto);
 
-  @Get()
-  findAll() {
-    return this.lessonService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.lessonService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLessonDto: UpdateLessonDto) {
-    return this.lessonService.update(+id, updateLessonDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.lessonService.remove(+id);
+    return {
+      lesson: lesson,
+      message: `Aula do dia ${createLessonDto.lessonDate} cadastrada com sucesso!`,
+    };
   }
 }
--- src/modules/lesson/lesson.service.ts
+++ src/modules/lesson/lesson.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateLessonDto } from './dto/create-lesson.dto';
-import { UpdateLessonDto } from './dto/update-lesson.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lesson } from './lesson.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class LessonService {
-  create(createLessonDto: CreateLessonDto) {
-    return 'This action adds a new lesson';
-  }
+  constructor(
+    @InjectRepository(Lesson)
+    private readonly lessonRepository: Repository<Lesson>,
+  ) {}
 
-  findAll() {
-    return `This action returns all lesson`;
-  }
+  async create(createLessonDto: CreateLessonDto) {
+    const lesson = new Lesson();
 
-  findOne(id: number) {
-    return `This action returns a #${id} lesson`;
-  }
+    lesson.title = createLessonDto.title;
+    lesson.startTime = createLessonDto.startTime;
+    lesson.endTime = createLessonDto.endTime;
+    lesson.lessonDate = createLessonDto.lessonDate;
+    lesson.observations = createLessonDto.observations;
 
-  update(id: number, updateLessonDto: UpdateLessonDto) {
-    return `This action updates a #${id} lesson`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} lesson`;
+    return this.lessonRepository.save(lesson);
   }
 }
